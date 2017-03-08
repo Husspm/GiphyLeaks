55cc06aab28fa8bcc3f6228ed3645cda2e2484fc
Patrick Hussey <husspm@gmail.com>
2017-03-08 15:53:01 -0500

    adding features

--- assets/logic.js
+++ assets/logic.js
@@ -1,5 +1,38 @@
 $(document).ready(function() {
-    var a = 10;
-    console.log(a);
-    $("h1").css("font-size", "80px");
+    //creating an array to contain all possible options
+    var topics = ["leek", "onion", "tomato", "carrots", "broccoli", "jalapenos", "potato"];
+    //using a for loop to generate buttons accoring to the number of items in topics
+    function makeButtons() {
+        for (index = 0; index < topics.length; index++) {
+            var buttonCreate = $("<button>").addClass("options");
+            buttonCreate.attr("data-name", topics[index]);
+            buttonCreate.text(topics[index]);
+            $(".buttonDiv").append(buttonCreate);
+        } //< ends for loop
+    } //< ends makeButtons();
+    makeButtons();
+    $(".options").on("click", function() {
+        $(".imagesDiv").empty();
+        var searchWord = $(this).attr("data-name");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+            searchWord + "&api_key=dc6zaTOxFJmzC&limit=10";
+        $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+            .done(function(response) {
+                console.log(response);
+                console.log(queryURL);
+                results = response.data;
+                for (i = 0; i < results.length; i++) {
+                    var imageView = $("<img>");
+                    imageView.attr("src", results[i].images.fixed_height.url);
+                    $(".imagesDiv").prepend(imageView);
+                }
+            });
+    });
+
+
+
+
 });
