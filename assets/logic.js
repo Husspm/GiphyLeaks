b8153963a5ace1c697929253850f63373a867ebd
Patrick Hussey <husspm@gmail.com>
2017-03-10 17:38:26 -0500

    added an s

--- assets/logic.js
+++ assets/logic.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
     $(document).on("click", ".options", function goToGiphy() {
         $(".imagesDiv").empty();
         var searchWord = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
             searchWord + "&api_key=dc6zaTOxFJmzC&limit=10";
         $.ajax({
                 url: queryURL,
